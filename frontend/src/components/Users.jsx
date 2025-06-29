238ad544051a8cabac2c4f7d98667577e8f96a22
Kanak-Pandey <kanakpandey302004@gmail.com>
2025-06-29 18:29:40 +0530

    Update Users.jsx

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -6,7 +6,7 @@ export function Users(){
     const [users,setUsers]=useState([]);
     const [filter,setFilter]=useState("");
     useEffect(()=>{
-        axios.get("http://localhost:3000/api/v1/user/bulk?filter="+filter)
+        axios.get("https://moneytransfer-09mf.onrender.com/api/v1/user/bulk?filter="+filter)
         .then(response=>{
             setUsers(response.data.user)
         })
